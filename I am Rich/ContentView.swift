a74c6774f8e6aeed5cdc9ac673cbcbe1747f1c41
potaticus <lordpotaticus@gmail.com>
2025-05-25 07:28:17 -0600

    Initial Commit

---
+++ I am Rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I am Rich
+//
+//  Created by Dallyn R. Christy on 5/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by Dallyn R. Christy on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
